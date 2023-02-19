b58e92685b1ed8cb4dfd644ba4ab5ee4dfde0596
xhablau <igor.gavioligastaldi@gmail.com>
2023-02-19 15:00:33 -0300

    responsividade aboutus

--- src/Home/About Us/AboutUS.tsx
+++ src/Home/About Us/AboutUS.tsx
@@ -16,7 +16,7 @@ function AboutUs() {
 
     return (
         <div>
-            
+
             <HeaderHome />
             <div style={{ backgroundImage: `url(${academia})`, backgroundSize: 'cover' }} id='imgAboutUs'>
                 <div id='txtAboutUsPrincipal'>
@@ -24,11 +24,13 @@ function AboutUs() {
                         Sobre Nós
                     </h1>
                     <div id='txtAboutUs'>
-                        O nosso site foi criado com o objetivo de ajudar as pessoas a alcançar seus objetivos de fitness de forma eficiente e personalizada.
-                        Nós acreditamos que todos merecem ter acesso a um programa de treinamento e dieta de alta qualidade,
-                        e é por isso que usamos a tecnologia da inteligência artificial para criar planos personalizados para cada um de nossos usuários.
-                        Não importa qual seja o seu objetivo - seja perder peso, ganhar músculos ou apenas ficar mais saudável -,
-                        nós estamos aqui para ajudá-lo a chegar lá. Não perca mais tempo e junte-se a nós agora mesmo. Juntos, vamos transformar sua vida!
+                        
+                            O nosso site foi criado com o objetivo de ajudar as pessoas a alcançar seus objetivos de fitness de forma eficiente e personalizada.
+                            Nós acreditamos que todos merecem ter acesso a um programa de treinamento e dieta de alta qualidade,
+                            e é por isso que usamos a tecnologia da inteligência artificial para criar planos personalizados para cada um de nossos usuários.
+                            Não importa qual seja o seu objetivo - seja perder peso, ganhar músculos ou apenas ficar mais saudável -,
+                            nós estamos aqui para ajudá-lo a chegar lá. Não perca mais tempo e junte-se a nós agora mesmo. Juntos, vamos transformar sua vida!
+                        
                     </div>
                 </div>
 
