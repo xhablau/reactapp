d0b4658e6cce5a84761c6e3c86fc5f5d4ea419b1
xhablau <igor.gavioligastaldi@gmail.com>
2023-03-01 19:55:57 -0300

    parte final

--- src/Planos/IsPremium.tsx
+++ src/Planos/IsPremium.tsx
@@ -1,46 +1,56 @@
 import HeaderHomePage from "../HomePage/HeaderHomePage/HeaderHomePage"
+import premiumimg from '../img/premium.jpeg'
 
 
 
 
 function IsPremium() {
-    const photoUser = JSON.parse(sessionStorage.getItem('photoUser') || '{}');
-    const emailUser = JSON.parse(sessionStorage.getItem('emailUser') || '{}');
-    const uidUser = JSON.parse(sessionStorage.getItem('uidUser') || '{}');
-    const displayName = JSON.parse(sessionStorage.getItem('displayName') || '{}');
-    const diffDays = JSON.parse(sessionStorage.getItem('diffDays') || '{}');
+  const photoUser = JSON.parse(sessionStorage.getItem('photoUser') || '{}');
+  const emailUser = JSON.parse(sessionStorage.getItem('emailUser') || '{}');
+  const uidUser = JSON.parse(sessionStorage.getItem('uidUser') || '{}');
+  const displayName = JSON.parse(sessionStorage.getItem('displayName') || '{}');
+  const diffDays = JSON.parse(sessionStorage.getItem('diffDays') || '{}');
 
-    const isMensal = JSON.parse(sessionStorage.getItem('isMensal') || '{}');
-    const isTrimestral = JSON.parse(sessionStorage.getItem('isTrimestral') || '{}');  
-    const isSemestral = JSON.parse(sessionStorage.getItem('isSemestral') || '{}');
-     let mensalidade
+  const isMensal = JSON.parse(sessionStorage.getItem('isMensal') || '{}');
+  const isTrimestral = JSON.parse(sessionStorage.getItem('isTrimestral') || '{}');
+  const isSemestral = JSON.parse(sessionStorage.getItem('isSemestral') || '{}');
+  let mensalidade
+  let dias
 
-    if (isMensal === true ){
-      mensalidade = " no plano Mensal"
-    } else if (isTrimestral === true) {
-      mensalidade = " no plano Trimestral"
-    } else if (isSemestral === true) {
-      mensalidade = " no plano Semestral"
-    } else{
-      mensalidade =""
-    }
+  if (isMensal === true) {
+    mensalidade = " no plano Mensal"
+  } else if (isTrimestral === true) {
+    mensalidade = " no plano Trimestral"
+  } else if (isSemestral === true) {
+    mensalidade = " no plano Semestral"
+  } else {
+    mensalidade = ""
+  }
+  console.log("oie", diffDays)
+
+  if (diffDays === null) {
+    dias = ""
+  } else {
+    dias = "Falta " + diffDays + " dias para terminar o seu premium."
+  }
 
 
-    return (
+  return (
 
-        <div >
-          <HeaderHomePage
-          photoUser={photoUser}
-          emailUser={emailUser}
-          uidUser={uidUser}
-          displayName={displayName}/>
-          <div>
-            {displayName} você é usuário Premium{mensalidade}! Falta {diffDays} dias para terminar o seu premium. 
-          </div>
+    <div >
+      <HeaderHomePage
+        photoUser={photoUser}
+        emailUser={emailUser}
+        uidUser={uidUser}
+        displayName={displayName} />
+      <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+        <img src={premiumimg}></img>
+        <h3>{displayName} você é usuário Premium{mensalidade}!  {dias}  </h3>
+      </div>
 
-        </div>
+    </div>
 
-    )
+  )
 }
 
 export default IsPremium
